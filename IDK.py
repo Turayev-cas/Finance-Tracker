75aa4778765eea83deb8db6081d8df8d62981b1e
Turayev-cas <Turayev.edu@proton.me>
2025-12-10 14:14:18 +0500

    Delete IDK.py

--- IDK.py
+++
@@ -1,9 +0,0 @@
-color = input("Enter color: ")
-plural_noun = input("Enter noun: ")
-name = input("Enter Name or celebrity: ")
-
-
-
-print ("Roses are " + color)
-print ( plural_noun + " are blue" )
-print ("I love " + name  )
